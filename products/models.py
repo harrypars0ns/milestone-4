8bc9df0621ed63d4fca8966c108fef0a7c144ce6
harrypars0ns <harry.scound@hotmail.com>
2020-08-06 13:37:36 +0000

    Created product and category models

---
+++ products/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    nation_of_origin = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
