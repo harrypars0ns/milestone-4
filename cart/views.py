5c078b9aef87892ebc9207f8bc263cce370b23ca
harrypars0ns <harry.scound@hotmail.com>
2020-08-14 13:09:22 +0000

    Added cart functionality FIXED, now showing items in cart.html

--- cart/views.py
+++ cart/views.py
@@ -12,6 +12,19 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Select a quantity of an item and add it to the cart """
 
+    # quantity = int(request.POST.get('quantity'))
+    # redirect_url = request.POST.get('redirect_url')
+    # cart = request.session.get('cart', {})
+
+    # if item_id in list(cart.keys()):
+    #     cart[item_id] += quantity
+    # else:
+    #     cart[item_id] = quantity
+
+    # request.session['cart'] = cart
+    # print(request.session['cart'])
+    # return redirect(redirect_url)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -24,3 +37,4 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     print(request.session['cart'])
     return redirect(redirect_url)
+    
