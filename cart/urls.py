57751be3dbcee79dbc45eb3ce4af60f0d3308d7d
harrypars0ns <harry.scound@hotmail.com>
2020-08-21 12:55:39 +0000

    Added edit quantity in cart / can now remove items

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
-]
+    path('edit/<item_id>/', views.edit_cart, name='edit_cart'),
+    ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -12,19 +12,6 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Select a quantity of an item and add it to the cart """
 
-    # quantity = int(request.POST.get('quantity'))
-    # redirect_url = request.POST.get('redirect_url')
-    # cart = request.session.get('cart', {})
-
-    # if item_id in list(cart.keys()):
-    #     cart[item_id] += quantity
-    # else:
-    #     cart[item_id] = quantity
-
-    # request.session['cart'] = cart
-    # print(request.session['cart'])
-    # return redirect(redirect_url)
-
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -35,6 +22,30 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
-    
+
+
+def edit_cart(request, item_id):
+    """ Select a quantity of an item and add it to the cart """
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        del cart[item_id]
+
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def delete_from_cart(request, item_id):
+    """ Select a quantity of an item and add it to the cart """
+
+    cart = request.session.get('cart', {})
+    del cart[item_id]
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
