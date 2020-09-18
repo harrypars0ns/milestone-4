90b2500faa5921fbba65d2b227625ea38da496e1
harrypars0ns <harry.scound@hotmail.com>
2020-09-18 12:50:38 +0000

    Finished About page

--- cart/static/cart/js/cart.js
+++ cart/static/cart/js/cart.js
@@ -1,11 +1,39 @@
-$('.remove-item').click(function(e) {
-        var csrfToken = "{{ csrf_token }}";
-        var itemId = $(this).attr('id').split('remove_')[1];
-        var url = `/cart/remove_button/${itemId}/`;
-        var data = {'csrfmiddlewaretoken': csrfToken};
+function removeItem(buttonId){
+    let clickOnButton = document.getElementById(buttonId)
+    let itemId = clickOnButton.dataset.item_id
+    let quantityInputElement = document.querySelector(
+        `input[data-item_id="${itemId}"]`)
+    quantityInputElement.value = 0;
+    let form = document.querySelector(`form[data-item_id="${itemId}"]`)
+    // if (clickOnButton){
+    //     alert("Got Button")
+    // }if (quantityInputElement){
+    //     alert("Got Input")
+    // }if (form){
+    //     alert("Got Form")
+    form.submit()  
+}
 
-        $.post(url, data)
-         .done(function() {
-             location.reload();
-         });
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+// $('.remove-item').click(function(e) {
+//         var csrfToken = "{{ csrf_token }}";
+//         var itemId = $(this).attr('id').split('remove_')[1];
+//         var url = `/cart/remove_button/${itemId}/`;
+//         var data = {'csrfmiddlewaretoken': csrfToken};
+
+//         $.post(url, data)
+//          .done(function() {
+//              location.reload();
+//          });
+//     })
