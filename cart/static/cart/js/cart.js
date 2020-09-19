4983757b5bc5e36d156cd268217f0ffd9fd92629
harrypars0ns <harry.scound@hotmail.com>
2020-09-19 19:25:17 +0000

    validated Javascript

--- cart/static/cart/js/cart.js
+++ cart/static/cart/js/cart.js
@@ -1,12 +1,12 @@
 // Function to remove an item from the cart with a button. Sets the quantity to 0 and saves. This is for good UX 
 function removeItem(buttonId){
-    let clickOnButton = document.getElementById(buttonId)
-    let itemId = clickOnButton.dataset.item_id
+    let clickOnButton = document.getElementById(buttonId);
+    let itemId = clickOnButton.dataset.item_id;
     let quantityInputElement = document.querySelector(
-        `input[data-item_id="${itemId}"]`)
+        `input[data-item_id="${itemId}"]`);
     quantityInputElement.value = 0;
-    let form = document.querySelector(`form[data-item_id="${itemId}"]`)
-    form.submit()  
+    let form = document.querySelector(`form[data-item_id="${itemId}"]`);
+    form.submit();
 }
 
 
